3cf7a9b63f0146dd5464fd30c884d445be960d73
kaushalgajjar777 <79360200+kaushalgajjar777@users.noreply.github.com>
2023-07-04 09:58:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BillingSystem from './Component/BillingSystem';
+import Languagechange from './Component/Languagechange';
+import Languagedata from './Component/Languagedata';
+
+function App() {
+  return (
+    <div>
+      <BillingSystem /> 
+      {/* <Languagedata/> */}
+    </div>
+  );
+}
+export default App;
